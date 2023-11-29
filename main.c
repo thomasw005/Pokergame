0b2e1e25bdb4d529c9e3cd93955e5d77b09a9817
BuildTools <unconfigured@null.spigotmc.org>
2023-11-29 14:51:34 -0500

    Cleaned up code and added comments.

--- main.c
+++ main.c
@@ -1,12 +1,25 @@
+// Thomas Wilson
+// 11/29/2023
+// Lecture 5 Assignment 1 Program 1
+// This program fills a deck of cards, picks a hand of 5 cards randomly from the deck twice, and then decides who has the better hand according to poker rules.
+
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-// Test1
+
+// Common numbers for deck length, number of suits/faces, and size of hand.
+#define CARDSINDECK 52
+#define SUITS 4
+#define FACES 13
+#define HANDSIZE 5
+
+// Card structure, contains what type of suit and face the card has according to an integer value (suit 1-4)(face 1-13).
 struct card{
     int cardSuit;
     int cardFace;
 };
 
+// Enum containing types of hands in poker.
 enum handType {
     RoyalFlush,
     StraightFlush,
@@ -20,50 +33,58 @@ enum handType {
     HighCard
 };
 
-void displayDeck(struct card array[], int deckLength);
+void drawHand(struct card deck[], int suitsInHand[], int facesInHand[], int* deckLength);
 void removeElement(struct card deck[],int position, int* deckLength);
 void displayHand(int suits[], int faces[]);
-void drawHand(struct card deck[], int suitsInHand[], int facesInHand[], int* deckLength);
+void displayDeck(struct card array[], int deckLength);
 enum handType analyzeHand(int suits[], int faces[]);
-
 
 int main() {
 
     // Set the seed for random() according to time.
     srand(time(NULL));
 
-    // Set deck length.
-    struct card deck[52];
-    int deckLength = 52;
+    // Creates 52 objects of structure type card. Sets deck length to 52.
+    struct card deck[CARDSINDECK];
+    int deckLength = CARDSINDECK;
 
+    // Create and initialize hand one.
     int suitsInHandOne[] = {0, 0 ,0 ,0};
     int suitsInHandTwo[] = {0, 0 ,0 ,0};
 
+    // Create and initialize hand two.
     int facesInHandOne[] = {0, 0 ,0 ,0,0,0,0,0,0,0,0,0,0};
     int facesInHandTwo[] = {0, 0 ,0 ,0,0,0,0,0,0,0,0,0,0};
 
 
     // Fill deck with cards.
     for (int i = 0; i < deckLength; i++) {
-        deck[i].cardSuit = i / 13 + 1;
-        deck[i].cardFace = i % 13 + 1;
+        deck[i].cardSuit = i / FACES + 1;
+        deck[i].cardFace = i % FACES + 1;
     }
 
-    // Draw five cards from deck.
+    // Display starting deck.
+    // displayDeck(deck, deckLength);
+
+    // Draw first hand of five cards from deck.
     drawHand(deck, suitsInHandOne, facesInHandOne, &deckLength);
     printf("HAND ONE:\n");
     displayHand(suitsInHandOne,facesInHandOne);
+
+    // Checks type of hand and saves it as an enum.
     enum handType handOne = analyzeHand(suitsInHandOne, facesInHandOne);
-    printf("%d\n", handOne);
     printf("\n");
 
+    // Draw second hand of five cards from deck.
     drawHand(deck, suitsInHandTwo, facesInHandTwo, &deckLength);
     printf("HAND TWO:\n");
     displayHand(suitsInHandTwo,facesInHandTwo);
+
+    // Checks type of hand and saves it as an enum.
     enum handType handTwo = analyzeHand(suitsInHandTwo, facesInHandTwo);
-    printf("%d\n", handTwo);
     printf("\n");
 
+    // Compare enum values of each hand to determine which one wins according to position within enum structure.
     if (handOne < handTwo) {
         printf("HAND ONE WINS\n");
     } else if (handTwo < handOne) {
@@ -72,37 +93,54 @@ int main() {
         printf("DRAW\n");
     }
 
-//    TEST CASES.
-//    printf("EXPECTING FLUSH(4)\n");
-//    int suitsInFlush[] = {0, 5, 0, 0};
-//    int facesInFlush[] = {1, 0,1,0,0,1,1,0,1,0,0,0,0};
-//    enum handType test = analyzeHand(suitsInFlush, facesInFlush);
-//    printf("ENUM IS %d\n", test);
-//
-//
-//    printf("EXPECTING STRAIGHT(6)\n");
-//    int suitsInStraight[] = {1, 4, 0, 0};
-//    int facesInStraight[] = {1, 1,1,1,1,0,0,0,0,0,0,0,0};
-//    test = analyzeHand(suitsInStraight, facesInStraight);
-//    printf("ENUM IS %d\n", test);
-//
-//
-//    printf("EXPECTING STRAIGHT FLUSH(1)\n");
-//    int suitsInStraightFlush[] = {0, 5, 0, 0};
-//    int facesInStraightFlush[] = {1, 1,1,1,1,0,0,0,0,0,0,0,0};
-//    test = analyzeHand(suitsInStraightFlush, facesInStraightFlush);
-//    printf("ENUM IS %d\n", test);
-//
-//
-//    printf("EXPECTING ROYALFLUSH(0)\n");
-//    int suitsInRoyalFlush[] = {0, 5, 0, 0};
-//    int facesInRoyalFlush[] = {1, 0,0,0,0,0,0,0,0,1,1,1,1};
-//    test = analyzeHand(suitsInRoyalFlush, facesInRoyalFlush);
-//    printf("ENUM IS %d\n", test);
+    // Display final deck.
+    // displayDeck(deck, deckLength);
 
     return 0;
 }
 
+// Chooses a random card from the deck and updates the hand depending on which card was pulled.
+// Removes that card from the deck and makes "deckLength" smaller.
+void drawHand(struct card deck[], int suitsInHand[], int facesInHand[], int* ptrDeckLength) {
+    int position = 0;
+    for (int i = 0; i < HANDSIZE; i++) {
+        position = rand() % *ptrDeckLength;
+        suitsInHand[deck[position].cardSuit - 1] += 1;
+        facesInHand[deck[position].cardFace - 1] += 1;
+        removeElement(deck, position, ptrDeckLength);
+        (*ptrDeckLength)--;
+    }
+}
+
+// Starting at the position where the card is removed, shuffles all cards after one spot forward, effectively "shortening" the list.
+void removeElement(struct card deck[], int position, int* ptrDeckLength) {
+    for (int i = position; i < *ptrDeckLength - 1; i++) {
+        deck[i] = deck[i + 1];
+    }
+}
+
+// Loops through suits and faces array, outputting the values for each integer.
+void displayHand(int suits[], int faces[]) {
+    for (int i = 0; i < SUITS; i++) {
+        printf("%d ", suits[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < FACES; i++) {
+        printf("%d ", faces[i]);
+    }
+    printf("\n");
+}
+
+// Loops through deck card array and prints out each objects suit and face.
+void displayDeck(struct card array[], int deckLength) {
+    printf("CURRENT DECK:\n");
+    for (int i = 0; i < deckLength; i++) {
+        printf("[%d, %d]\n", array[i].cardSuit, array[i].cardFace);
+    }
+    printf("\n\n");
+}
+
+// Takes in a hand's suits and faces array, then returns an enum of what type of hand it is.
 enum handType analyzeHand(int suits[], int faces[]) {
     int num_consec = 0;
     int rank, suit;
@@ -113,44 +151,40 @@ enum handType analyzeHand(int suits[], int faces[]) {
     bool three = false;
     int pairs = 0;
 
-    // check for flush – 5 cards of the same suit
-    for (suit = 0; suit < 4; suit++) {
-        if (suits[suit] == 5) {
+    // Checks for a flush – five cards of the same suit.
+    for (suit = 0; suit < SUITS; suit++) {
+        if (suits[suit] == HANDSIZE) {
             flush = true;
         }
     }
 
-    // Check for royal - straight of 10-ace.
-    for (rank = 9; rank < 13 && faces[rank]; rank++) {
+    // Checks for a royal - straight of 10-ace.
+    for (rank = 9; rank < FACES && faces[rank]; rank++) {
         num_consec++;
     }
-
-    if (num_consec == 4 && faces[0] == 1) {
+    if (num_consec == SUITS && faces[0] == 1) {
         royal = true;
     }
 
-
-
-
-// check for straight – eg. One each of 5,6,7,8,9
-// locate the first card
+    // Locates the first card.
+    // Checks for a straight – one each of 5,6,7,8,9.
     rank = 0;
     num_consec = 0;
     while (faces[rank] == 0) {
         rank++;
 
     }
-// count the consecutive non-zero faces
-    for (; rank < 13 && faces[rank]; rank++) {
+
+    // Counts the consecutive non-zero faces.
+    for (; rank < FACES && faces[rank]; rank++) {
         num_consec++;
     }
-
-    if (num_consec == 5) {
+    if (num_consec == HANDSIZE) {
         straight = true;
     }
 
-    //* check for 4-of-a-kind, 3-of-a-kind, and pairs */
-    for (rank = 0; rank < 13; rank++) {
+    // Checks for four of a kind, three of a kind, and how many pairs there are.
+    for (rank = 0; rank < FACES; rank++) {
         switch (faces[rank]) {
             case 4:
                 four = true;
@@ -164,7 +198,7 @@ enum handType analyzeHand(int suits[], int faces[]) {
         }
     }
 
-    // Check what type of hand it is and return.
+    // Check what type of hand it is and return enum value.
     if (royal && flush) {
         printf("ROYAL FLUSH\n");
         return RoyalFlush;
@@ -196,47 +230,4 @@ enum handType analyzeHand(int suits[], int faces[]) {
         printf("HIGH CARD\n");
         return HighCard;
     }
-}
-
-void drawHand(struct card deck[], int suitsInHand[], int facesInHand[], int* ptrDeckLength) {
-    int position = 0;
-    for (int i = 0; i < 5; i++) {
-        position = rand() % *ptrDeckLength;
-        suitsInHand[deck[position].cardSuit - 1] += 1;
-        facesInHand[deck[position].cardFace - 1] += 1;
-        removeElement(deck, position, ptrDeckLength);
-        (*ptrDeckLength)--;
-    }
-}
-
-void displayDeck(struct card array[], int deckLength) {
-    printf("CURRENT DECK:\n");
-    for (int i = 0; i < deckLength; i++) {
-        printf("[%d, %d]\n", array[i].cardSuit, array[i].cardFace);
-    }
-    printf("\n\n");
-
-}
-
-void removeElement(struct card deck[], int position, int* ptrDeckLength) {
-    if (position >= *ptrDeckLength)
-    {
-        printf ("\nError");
-    }
-    else {
-        for (int i = position; i < *ptrDeckLength - 1; i++) {
-            deck[i] = deck[i + 1];
-        }
-    }
-}
-
-void displayHand(int suits[], int faces[]) {
-    for (int i = 0; i < 4; i++) {
-        printf("%d ", suits[i]);
-    }
-    printf("\n");
-    for (int i = 0; i < 13; i++) {
-        printf("%d ", faces[i]);
-    }
-    printf("\n");
 }
